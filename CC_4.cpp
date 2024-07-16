a5deac2f9698ff0cdd663f0716e1afce2fa1326d
Deviitt11 <david.collados11@gmail.com>
2024-07-16 19:51:37 +0200

    Intro CPP - 16jul (v2)

---
+++ CC_4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   string name = "James";
+   int age = 42;
+   double height = 185.7;
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age;
+    cout << " | Height: " << height;
+}
